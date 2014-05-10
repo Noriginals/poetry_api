37afa7c0f5fc41ad5a62157c59b97f6d09b809df
Naqeeb Memon <naqeeb.memon@gmail.com>
2014-05-10 17:07:29 -0400

    Added decimal check for letter ratio

--- api/forms.py
+++ api/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import TextField, BooleanField, DecimalField
+from wtforms.validators import DataRequired, NumberRange
 
 class PoetrySearchForm(Form):
     keyword = TextField('Keywords', validators=[DataRequired()])
     url = BooleanField('Remove URLs')
     emoji = BooleanField('Remove Emoji')
-    letter_ratio = TextField('Letter Ratio') # 0 to 1
+    letter_ratio = DecimalField('Letter Ratio', places=3, validators=[NumberRange(0, 1)]) # 0 to 1
     syllabus = TextField('Syllabus Format')
     blacklist = TextField('Blacklist')
